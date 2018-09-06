02ed19373f15ebece88b046a06bd031af9953ab7
lbarret4 <llewellynbar4@gmail.com>
2018-09-06 19:34:49 -0400

    Added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+
+
+
+
+
+
+
+const Header= (props) => { 
+
+    return (
+        <div className="jumbotron bg-warning d-flex justify-content-center">
+        <img src={props.logo} alt="Logo of Studio Ghibli " />
+    </div>
+    );
+
+
+
+
+}
+
+export default Header;
