2f3f84c049fcdeab7913df10f0f84d1bad2721b3
lbarret4 <llewellynbar4@gmail.com>
2018-09-06 22:39:15 -0400

    Phase 3 done and added loadData to retrieve data from specified route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import 'es6-promise';
-import Card from './Card'
+import FilmCard from './FilmCard'
+import PeopleCard from './PeopleCard'
 import logo from '../logo.png'
+import Header from './Header'
 
 class App extends Component {
 
@@ -10,59 +12,110 @@ class App extends Component {
         super(props);
         this.state = {
             data: [],
-            hasLoadedFilms: false
+            hasLoaded: false,
+            cardtype: "",
+            isLoading: false
         }
-        this.handleLoadFilms=this.handleLoadFilms.bind(this);
+        this.handleLoaded = this.handleLoaded.bind(this);
+        this.handleBack = this.handleBack.bind(this);
     }
 
-    async componentDidMount() {
-        try {
-            let results = await fetch("https://ghibliapi.herokuapp.com/films");
-            results = await results.json();
+    /*   async componentDidMount() {
+          try {
+              let results = await fetch("https://ghibliapi.herokuapp.com/films");
+              results = await results.json();
+  
+              this.setState({
+                  data: results
+              })
+  
+          } catch (error) {
+              console.log(error);
+          }
+  
+      } */
 
+    async loadData(field) {
+        let url = `https://ghibliapi.herokuapp.com/${field}`;
+        try {
+            let results = await fetch(url);
+            results = await results.json();
             this.setState({
-                data: results
+                hasLoaded: true,
+                data: results,
+                isLoading: false
             })
 
         } catch (error) {
             console.log(error);
         }
-
     }
 
-    handleLoadFilms(e){
+    handleLoaded(field, e) {
+        e.preventDefault();
+        this.loadData(field);
+        this.setState({
+            cardtype: field,
+            isLoading: true
+        })
+
+    }
+    
+    handleBack(e){
         e.preventDefault();
         this.setState({
-            hasLoadedFilms:true
+            hasLoaded:false
         })
 
     }
 
-    render() {
-        let films = this.state.data.map((film) => {
-            return <Card title={film.title} rt={film.rt_score} desc={film.description} direc={film.director} date={film.release_date} key={film.id} />
-        });
 
-        if (this.state.hasLoadedFilms) {
+
+    render() {
+        let cards;
+        if (this.state.cardtype === "films") {
+            cards = this.state.data.map((film) => {
+                return <FilmCard title={film.title} rt={film.rt_score} desc={film.description} direc={film.director} date={film.release_date} key={film.id} />
+            });
+        } else if (this.state.cardtype === "people") {
+            cards = this.state.data.map((people) => {
+                
+                return <PeopleCard name={people.name} gender={people.gender} age={people.age} hair={people.hair_color} eyes={people.eye_color} id={people.id} key={people.id}/>
+
+
+            })
+        }
+
+
+        if (this.state.hasLoaded) {
             return (
 
                 <div className="container">
-                    <div className="jumbotron bg-warning d-flex justify-content-center">
-                        <img src={logo} alt="Logo of Studio Ghibli " />
-                    </div>
+                    < Header logo={logo} />
+                    <button type="button" className="btn btn-dark mb-2" onClick={this.handleBack}>Go Back</button>
                     <div className="card-columns">
-                        {films}
+                        {cards}
                     </div>
                 </div>
             );
+        } else if (this.state.isLoading) {
+            return (
+                <div className="container">
+                    < Header logo={logo} />
+                    <h1 className="display-1">Loading....</h1>
+                </div>
+            );
+
         } else {
             return (
 
                 <div className="container">
-                    <div className="jumbotron bg-warning d-flex justify-content-center">
-                        <img src={logo} alt="Logo of Studio Ghibli " />
+                    < Header logo={logo} />
+                    <div className="btn-group" role="group">
+                        <button type="button" className="btn btn-dark mb-2" onClick={this.handleLoaded.bind(this, 'films')}>Load Films</button>
+                        <button type="button" className="btn btn-dark mb-2" onClick={this.handleLoaded.bind(this, 'people')}>Load People</button>
                     </div>
-                    <button type="button" className="btn btn-dark mb-2" onClick={this.handleLoadFilms}>Load Films</button>
+
                 </div>
 
 
